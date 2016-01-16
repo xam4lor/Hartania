fc6676999db83bd6c151f4ba694b3cb094a43696
xam4lor <xam4lor@gmail.com>
2016-01-16 10:40:23 +0100

    ajout de la base de la conversation interactive

---
+++ src/fr/hartania/xam4lor/msgSystem/ConversationSystem.java
@@ -0,0 +1,13 @@
+package fr.hartania.xam4lor.msgSystem;
+
+import org.bukkit.entity.Player;
+
+public class ConversationSystem {
+	public static void convBegin(Player player1, Player player2) {
+		sendMessageBetw(player1, player2);
+	}
+
+	private static void sendMessageBetw(Player player1, Player player2) {
+		
+	}
+}
